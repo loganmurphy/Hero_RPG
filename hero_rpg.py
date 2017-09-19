3b847177c79fd41e703a86186e8cb022df2e23f2
loganmurphy <loganmurphy1984@gmail.com>
2017-09-19 16:23:05 -0500

    Hero_RPG_v8

--- hero_rpg.py
+++ hero_rpg.py
@@ -1,6 +1,5 @@
-# The methods attack and print_status method in Hero and Goblin look
-# almost identical, but not quite. Is it possible to move them into the
-# Character class as well? Give it a try.
+# Create a zombie character that cannot die and have it fight
+# the hero instead of the goblin.
 
 class Character:
     def __init__(self, name, health, power):
@@ -13,54 +12,52 @@ class Character:
     def print_status(self):
         if self.name is "Hero":
             who = "You have"
-        else:
+        elif self.name is "goblin":
             who = "The goblin has"
+        else:
+            who = "The zombie has"
         print("{} {} health and {} power.".format(who, self.health, self.power))
     def attack(self, enemy):
-        if self.name == "Hero":
-            attacker = "You do"
-            attackee = "the goblin"
+        # I re-wrote this code to account for the zombie being invincible.
+        if enemy.name == "Zombie":
+            print("Your attack has no effect!")
+        elif self.name == "Zombie":
+            enemy.health -= self.power
+            print("The zombie does {} damage to you.".format(self.power))
+        elif self.name == "Hero":
+            enemy.health -= self.power
+            print("You do {} damage to goblin.".format(self.power))
         else:
-            attacker = "The goblin does"
-            attackee = "you"
-        enemy.health -= self.power
-        print("{} {} damage to {}.".format(attacker, self.power, attackee))
+            enemy.health -= self.power
+            print("The goblin does {} damage to you.".format(self.power))
 
 class Hero (Character):
     pass
-    # Everything is in the Character class now.
-    # def attack(self, goblin):
-    #     goblin.health -= hero.power
-    #     print("You do {} damage to the goblin.".format(hero.power))
-    # def print_status(self):
-    #     print("You have {} health and {} power.".format(hero.health, hero.power))
 
 class Goblin (Character):
     pass
-    # Everything is in the Character class now.
-    # def attack(self, hero):
-    #     hero.health -= goblin.power
-    #     print("The goblin does {} damage to you.".format(goblin.power))
-    # def print_status(self):
-    #     print("The goblin has {} health and {} power.".format(goblin.health, goblin.power))
+
+class Zombie (Character):
+    pass
 
 hero = Hero("Hero", 10, 5)
 goblin = Goblin("Goblin", 6, 2)
+zombie = Zombie("Zombie", 10, 2)
 
-while goblin.alive() and hero.alive():
+while zombie.alive() and hero.alive():
     hero.print_status()
-    goblin.print_status()
+    zombie.print_status()
     print()
     print("What do you want to do?")
-    print("1. fight goblin")
+    print("1. fight zombie")
     print("2. do nothing")
     print("3. flee")
     print("> ", end=' ')
     raw_input = input()
     if raw_input == "1":
-        hero.attack(goblin)
-        if goblin.health <= 0:
-            print("The goblin is dead.")
+        hero.attack(zombie)
+        if zombie.health <= 0:
+            print("The zombie is dead.")
     elif raw_input == "2":
         pass
     elif raw_input == "3":
@@ -68,7 +65,7 @@ while goblin.alive() and hero.alive():
         break
     else:
         print("Invalid input {}".format(raw_input))
-    if goblin.alive():
-        goblin.attack(hero)
+    if zombie.alive():
+        zombie.attack(hero)
         if hero.health <= 0:
             print("You are dead.")
