c6cad80df21645c94850858fb6d558e5e552e195
loganmurphy <loganmurphy1984@gmail.com>
2017-09-19 14:52:24 -0500

    Hero_RPG_v3

--- hero_rpg.py
+++ hero_rpg.py
@@ -1,6 +1,11 @@
-# Similarly, take the code for the goblin attacking the hero and extract it into a method
-# (also call it attack) of the Goblin class. Replace the existing code with a call to the attack method.
-# It should look like goblin.attack(hero).
+# Refactor the while condition:
+#
+# while goblin.health > 0 and hero.health > 0:
+# to
+#
+# while goblin.alive() and hero.alive():
+# The health checks should be moved to within the alive methods of
+# Hero and Goblin respectively.
 
 class Hero:
     def __init__(self, health, power):
@@ -9,7 +14,9 @@ class Hero:
     def attack(self, goblin):
         goblin.health -= hero.power
         print("You do {} damage to the goblin.".format(hero.power))
-
+    def alive(self):
+        if self.health > 0:
+            return True
 class Goblin:
     def __init__(self, health, power):
         self.health = health
@@ -17,10 +24,13 @@ class Goblin:
     def attack(self, hero):
         hero.health -= goblin.power
         print("The goblin does {} damage to you.".format(goblin.power))
+    def alive(self):
+        if self.health > 0:
+            return True
 hero = Hero(10, 5)
 goblin = Goblin(6, 2)
-
-while goblin.health > 0 and hero.health > 0:
+# This while statement was changed.
+while goblin.alive() and hero.alive():
     print("You have {} health and {} power.".format(hero.health, hero.power))
     print("The goblin has {} health and {} power.".format(goblin.health, goblin.power))
     print()
@@ -41,12 +51,8 @@ while goblin.health > 0 and hero.health > 0:
         break
     else:
         print("Invalid input {}".format(raw_input))
-
-    if goblin.health > 0:
-        # Goblin attacks hero
+# I changed the below if statement as well.
+    if goblin.alive():
         goblin.attack(hero)
-        # The abov line replaces the below line.
-        # hero.health -= goblin.power
-        # print("The goblin does {} damage to you.".format(goblin.power))
         if hero.health <= 0:
             print("You are dead.")
